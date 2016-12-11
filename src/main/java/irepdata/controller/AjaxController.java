e6c8883601c09d2a2e7d13f88cabdb7119820cea
Gvo3d <denis.yakimov89@gmail.com>
2016-12-11 12:54:55 +0200

    IDeaRepository v.0.0.41 WORKING: models, dao, service, tests, c3p0, jetty:embedded, remoteMysql connection, war packaging, some jsps, first ajax function
    CHANGES: ajax is working
    
    TODO: controllers insides, pages, pagination, ajax functions, security, make ajax javascripts work from external files
    Author: Gvo3d & Rus.maxx

--- src/main/java/irepdata/controller/AjaxController.java
+++ src/main/java/irepdata/controller/AjaxController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -37,7 +38,7 @@ public class AjaxController {
 
     @JsonView(JSONViews.List.class)
     @ResponseBody
-    @RequestMapping(value = PREFIX + "sortenabledideas")
+    @RequestMapping(value = PREFIX + "sortenabledideas", method= RequestMethod.POST)
     public ResponseEntity getSearchResultViaAjax(@RequestBody IdeaSortCriteria ideaSortCriteria) {
         logger.info("InAJAX controller");
         ResponseEntity resulting;

6119e1df3e80cded69158ab225e58ad41180b55a
Gvo3d <denis.yakimov89@gmail.com>
2016-12-27 14:45:42 +0200

    IDeaRepository v.0.0.81 WORKING: models, dao, service, tests, c3p0, jetty:embedded, war packaging, some jsps, authorizationByLoginAndPassword for maincontroller, logout, auth check interceptor, creation and edition of ideas, made redirect for editing not your own idea, comments showing correctly, all datas formatted correctly, comments created and showed correctly, idea can be watched, liked and disliked, likes stores in cookies, watches in session, session expires in 20 min of afk
    CHANGES: started to insert interface, fixed equals and hashcode funcs
    
    TODO: ajax, admin controller
    
    TRYING: tagsCloud AJAX
    
    Author: Gvo3d & Russ.max

--- src/sql/DataInjection.sql
+++ src/sql/DataInjection.sql
@@ -2,28 +2,32 @@ USE `idearepository`;
 
 insert into USERS (USER_NAME, LOGIN, PSWD) values ('System_Programmer', 'sysprog', 'sysprog');
 insert into USERS (USER_NAME, LOGIN, PSWD) values ('QwertyUser', 'qwerty', 'qwerty');
-insert into USERS (USER_NAME, LOGIN, PSWD) values ('mod', 'mod', 'mod');
-insert into USERS (USER_NAME, LOGIN, PSWD) values ('list', 'list', 'list');
+insert into USERS (USER_NAME, LOGIN, PSWD) values ('ModUser', 'mod', 'mod');
+insert into USERS (USER_NAME, LOGIN, PSWD) values ('ListUser', 'list', 'list');
 
 insert into CONTENTS (IDEA_CONTENT) values ('framework created by Gavin King in 2001.');
 insert into CONTENTS (IDEA_CONTENT) values ('It is a powerful.');
 insert into CONTENTS (IDEA_CONTENT) values ('Hibernate maps Java classes.');
 
-insert into IDEAS (IDEA_NAME, DESCRIPTION, IDEA_AUTHOR_ID, IDEA_CONTENT_ID) values ('idea', 'I hev idea', '1', '1');
-insert into IDEAS (IDEA_NAME, DESCRIPTION, IDEA_IMAGE, IDEA_AUTHOR_ID, IDEA_CONTENT_ID) values ('idea2', 'I hev idea2', 'kartinka', '2', '2');
-insert into IDEAS (IDEA_NAME, DESCRIPTION, IDEA_AUTHOR_ID, IDEA_CONTENT_ID) values ('idea3', 'I hev idea3', '3', '3');
+insert into IDEAS (IDEA_NAME, DESCRIPTION, IDEA_AUTHOR_ID, IDEA_CONTENT_ID) values ('Interface idea', 'I suggest to change our company name to CATACOMB GOBLINS', '1', '1');
+insert into IDEAS (IDEA_NAME, DESCRIPTION, IDEA_IMAGE, IDEA_AUTHOR_ID, IDEA_CONTENT_ID) values ('Оптимизация затрат', 'I hev idea2', 'kartinka', '2', '2');
+insert into IDEAS (IDEA_NAME, DESCRIPTION, IDEA_AUTHOR_ID, IDEA_CONTENT_ID) values ('NEW SUPPLIER!!!', 'New supplier wants to be our partner', '3', '3');
 
-insert into COMMENTS (COMMENT_AUTHOR_ID, COMMENT_IDEA_ID, COMMENT_CONTENT) values ('1', '1', 'Captain Obvious');
-insert into COMMENTS (COMMENT_AUTHOR_ID, COMMENT_IDEA_ID, COMMENT_CONTENT) values ('2', '1', 'Das is fantasnik');
-insert into COMMENTS (COMMENT_AUTHOR_ID, COMMENT_IDEA_ID, COMMENT_CONTENT) values ('3', '2', 'Ya ya');
+insert into COMMENTS (COMMENT_AUTHOR_ID, COMMENT_IDEA_ID, COMMENT_CONTENT) values ('1', '1', 'Idea is brilliant!');
+insert into COMMENTS (COMMENT_AUTHOR_ID, COMMENT_IDEA_ID, COMMENT_CONTENT) values ('2', '1', 'Первым делом уволить к чёрту всех кадровиков!');
+insert into COMMENTS (COMMENT_AUTHOR_ID, COMMENT_IDEA_ID, COMMENT_CONTENT) values ('3', '2', 'Where do you want to take money for that partnership?');
 
-insert into TAGS (TAG_CONTENT) values ('Sisk');
-insert into TAGS (TAG_CONTENT) values ('big_Sisk');
-insert into TAGS (TAG_CONTENT) values ('midl_Sisk');
-insert into TAGS (TAG_CONTENT) values ('small_Sisk');
-insert into TAGS (TAG_CONTENT) values ('bez_Sisek');
+insert into TAGS (TAG_CONTENT) values ('company');
+insert into TAGS (TAG_CONTENT) values ('common');
+insert into TAGS (TAG_CONTENT) values ('funds');
+insert into TAGS (TAG_CONTENT) values ('interface');
+insert into TAGS (TAG_CONTENT) values ('company_name');
 
 insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('1', '1');
+insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('4', '1');
+insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('5', '1');
 insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('1', '2');
-insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('2', '1');
-insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('3', '1');
+insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('3', '2');
+insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('1', '3');
+insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('2', '3');
+insert into TAG_MAGAZINE (TAG_MAG_ID, TAG_IDEA_ID) values ('3', '3');
